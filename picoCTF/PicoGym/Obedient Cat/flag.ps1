f241b9337871d7cf4b8fb45951f1ef9fccc4683d
CarlosAmericano <42471058+CarlosAmericano@users.noreply.github.com>
2022-04-01 19:42:47 +0100

    Upload
    Upload

--- picoCTF/PicoGym/Obedient Cat/flag.ps1
+++
@@ -1,3 +0,0 @@
-$flag=Invoke-RestMethod -Uri 'https://mercury.picoctf.net/static/fb851c1858cc762bd4eed569013d7f00/flag' -UseBasicParsing
-
-#picoCTF{s4n1ty_v3r1f13d_28e8376d}
---
+++ picoCTF/Unpack me/brute.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+# Tested using bash version 4.1.5
+rm log.log
+for ((i=1;i<=1000000;i++));
+do
+   # your-unix-command-here
+   echo $i >> log.log
+   echo $i | ./unpackme-upx >> log.log
+done
