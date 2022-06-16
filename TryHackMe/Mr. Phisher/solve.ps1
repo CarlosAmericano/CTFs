8b7440c655c50500cf5490f0c0e7447ffe978f7a
CarlosAmericano <42471058+CarlosAmericano@users.noreply.github.com>
2022-06-16 08:56:16 +0100

    Markdown fixes

--- TryHackMe/Mr. Phisher/solve.ps1
+++ TryHackMe/Mr. Phisher/solve.ps1
@@ -2,8 +2,6 @@ $array = (102, 109, 99, 100, 127, 100, 53, 62, 105, 57, 61, 106, 62, 62, 55, 110
 $flag=''
 $i=0
 foreach ($item in $array) {
-    #Write-Host $item
-    #Write-Host $i
     $x=[char]($item -bxor $i)
     $flag=$flag+$x
     $i=$i+1
